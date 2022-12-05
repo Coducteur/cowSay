df4e16b60fdd8922f650ca5f8cdf7c2dcd9a6467
Coducteur <chat.adr01@gmail.com>
2022-12-05 15:41:23 +0100

    don't need function for number of cows

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ console.log(
   })
 );
 
-function get_cows(error, cow_names) {
-    if (error) {
-        console.log(error)
-    }
-    else if (cow_names) {
-        console.log(`Number of cows available: ${cow_names.length}`);
-    }
-}
+// function get_cows(error, cow_names) {
+//     if (error) {
+//         console.log(error)
+//     }
+//     else if (cow_names) {
+//         console.log(`Number of cows available: ${cow_names.length}`);
+//     }
+// }
 
-cowsay.list(get_cows);
+// cowsay.list(get_cows);
